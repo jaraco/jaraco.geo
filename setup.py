1547e53932c15814faa8bfdf570a66aec506c6a0
jaraco <none@none>
2009-03-04 03:57:56 +0000

    Updated deployment to use long description

--- setup.py
+++ setup.py
@@ -2,46 +2,50 @@
 
 """ Setup script for building jaraco-util distribution
 
-Copyright © 2004-2008 Jason R. Coombs
+Copyright © 2004-2009 Jason R. Coombs
 """
 
 from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup, find_packages
-import pypi_fix
+from jaraco.util.package import read_long_description
 
 __author__ = 'Jason R. Coombs <jaraco@jaraco.com>'
 __version__ = '$Rev$'[6:-2]
 __svnauthor__ = '$Author$'[9:-2]
 __date__ = '$Date$'[7:-2]
 
-setup (name = 'jaraco.geo',
-		version = '1.0',
-		description = 'Geographic coordinates package',
-		author = 'Jason R. Coombs',
-		author_email = 'jaraco@jaraco.com',
-		url = 'http://www.jaraco.com/projects/jaraco.geo',
-		packages = find_packages(exclude=['ez_setup', 'tests', 'examples']),
-		namespace_packages = ['jaraco',],
-		license = 'MIT',
-		classifiers = [
-			"Development Status :: 4 - Beta",
-			"Intended Audience :: Developers",
-			"Programming Language :: Python",
-		],
-		entry_points = {
-			'console_scripts': [
-				],
-		},
-		install_requires=[
-			'jaraco.util',
-		],
-		extras_require = {
-		},
-		dependency_links = [
-		],
-		tests_require=[
-			'nose>=0.10',
-		],
-		test_suite = "nose.collector",
-	)
+name = 'jaraco.geo'
+
+setup (
+	name = name,
+	version = '1.0',
+	description = 'Geographic coordinates package',
+	long_description = read_long_description(__file__),
+	author = 'Jason R. Coombs',
+	author_email = 'jaraco@jaraco.com',
+	url = 'http://pypi.python.org/pypi/'+name,
+	packages = find_packages(exclude=['ez_setup', 'tests', 'examples']),
+	namespace_packages = ['jaraco',],
+	license = 'MIT',
+	classifiers = [
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Programming Language :: Python",
+	],
+	entry_points = {
+		'console_scripts': [
+			],
+	},
+	install_requires=[
+		'jaraco.util',
+	],
+	extras_require = {
+	},
+	dependency_links = [
+	],
+	tests_require=[
+		'nose>=0.10',
+	],
+	test_suite = "nose.collector",
+)
